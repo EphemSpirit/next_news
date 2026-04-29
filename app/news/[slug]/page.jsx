f60d4c9d02f4df76b685f35210017b809f3ac899
Drew Hund <languageinvestigator@gmail.com>
2026-04-29 17:04:31 -0400

    dummy data and basic page styling

---
+++ app/news/[slug]/page.jsx
@@ -0,0 +1,17 @@
+import { DUMMY_NEWS } from "@/dummy-news"
+
+const NewsDetailPage = ({ params }) => {
+  const newsItem = DUMMY_NEWS.find(item => item.slug === params.slug)
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  )
+}
+
+export default NewsDetailPage
